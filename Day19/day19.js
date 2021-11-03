5a2fff153af8193e31239c6e42520ff35ad9896d
Jenn Hernes <jenn_hernes@hotmail.com>
2021-11-03 15:45:54 +0200

    moving computers

--- Day19/day19.js
+++ Day19/day19.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
-let [rules, molecule] = fs.readFileSync(path.join(__dirname, 'input.txt'))
+let [rules, endresult] = fs.readFileSync(path.join(__dirname, 'input.txt'))
                 .toString()
                 .trim()
                 .split('\n\n')
@@ -18,24 +18,24 @@ function part1() {
     let result = [];
     for (let j = 0; j < rules.length; j++)
     {
-        for (let i = 0; i < molecule.length; i++)
+        for (let i = 0; i < endresult.length; i++)
         {
             if (j === 0)
             {
-                console.log(molecule.slice(0, i));
-                console.log(molecule.slice(i));
-                console.log(molecule.slice(i).replace(rules[j].start, rules[j].end));
+                console.log(endresult.slice(0, i));
+                console.log(endresult.slice(i));
+                console.log(endresult.slice(i).replace(rules[j].start, rules[j].end));
             }
-            result.push(molecule.slice(0,i) + molecule.slice(i).replace(rules[j].start, rules[j].end));
+            result.push(endresult.slice(0,i) + endresult.slice(i).replace(rules[j].start, rules[j].end));
         }
     }
 
     console.log(rules);
-    console.log(molecule);
+    console.log(endresult);
     console.log(result);
 
     const set = new Set(result.slice(0,-1));
-    set.delete(molecule);
+    set.delete(endresult);
     console.log(set);
     console.log(set.size);
 }
@@ -46,22 +46,34 @@ function part2()
     results.set(0, ['e']);
 
     let layer = 0;
-    while (true)
+    let created = false;
+    while (!created)
     {
-        let molecules = results.get(layer);
+        let molecules = results.get(layer) as Array();
+        console.log("Molecules: " + molecules);
+        if (layer === 1) break;
+        results.set(layer+1, new Array());
         for (const molecule of molecules)
         {
-            for (const rule in rules)
+            for (const rule of rules)
             {
-                let i = substring.indexOf(rule.start);
+                let i = molecule.indexOf(rule.start);
                 while (i > -1)
                 {
                     let substring = molecule.slice(i);
-                    let m = molecule.slice(0,i) + molecule.slice(i).replace(rule.start,rule.end);
-                    rules.set(layer+1, rules.get(layer+1).append(m));
-                    i = molecule.slice()
+                    let m = molecule.slice(0,i) + substring.replace(rule.start,rule.end);
+                    if (m === endresult)
+                    {
+                        console.log(layer+1);
+                        created = true;
+                    }
+                    results.set(layer+1, results.get(layer+1).push(m));
+                    i = m.indexOf(rule.start);
                 }
             }
         }
+        layer++;
     }
 }
+
+part2();
